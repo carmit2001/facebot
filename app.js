4086c1b998a44c7ee2ad6a50a39071ed98d240c5
Train10 <Train10@eladsystems.co.il>
2019-01-03 14:47:52 +0200

    change app.js

--- app.js
+++ app.js
@@ -208,7 +208,8 @@ function processMessage(event) {
 }
 
 function findMovie(userId, movieTitle) {
-    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
+  let apiKey ='420ff3f7'
+    request(`http://www.omdbapi.com/?type=movie&t=${movieTitle}&apiKey=${apiKey}`, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var movieObj = JSON.parse(body);
             if (movieObj.Response === "True") {
