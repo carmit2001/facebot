7e4b4c78867807964d24cb66c5c8d1ada6b15150
Train10 <Train10@eladsystems.co.il>
2019-01-21 11:50:06 +0200

    app.js

--- app.js
+++ app.js
@@ -1,101 +1,67 @@
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
-
-// Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
-});
-
-// Facebook Webhook
-// Used for verification
-app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
-  } else {
-    console.error("Verification failed. The tokens do not match.");
-    res.sendStatus(403);
-  }
-});
-
-// All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function (req, res) {
-  // Make sure this is a page subscription
-  if (req.body.object == "page") {
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        }
-      });
-    });
-
-    res.sendStatus(200);
-  }
-});
-
-function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
-
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "Welcome to our schedule!";
-      sendMessage(senderId, {text: message});
-    });
-  }
-}
-
-// sends message to user
-function sendMessage(recipientId, message) {
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
-    json: {
-      recipient: {id: recipientId},
-      message: message,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log("Error sending message: " + response.error);
-    }
-  });
-}
-
-// var express = require("express");
-// var request = require("request");
-// var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI);
-// mongoose.connect()
+const apiKey ='420ff3f7';
+
+// var app = express();
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.listen((process.env.PORT || 5000));
+
+// // Server index page
+// app.get("/", function (req, res) {
+//   res.send("Deployed!");
+// });
+
+// // Facebook Webhook
+// // Used for verification
+// app.get("/webhook", function (req, res) {
+//   if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+//     console.log("Verified webhook");
+//     res.status(200).send(req.query["hub.challenge"]);
+//   } else {
+//     console.error("Verification failed. The tokens do not match.");
+//     res.sendStatus(403);
+//   }
+// });
+
+// // All callbacks for Messenger will be POST-ed here
+// app.post("/webhook", function (req, res) {
+//   // Make sure this is a page subscription
+//   if (req.body.object == "page") {
+//     // Iterate over each entry
+//     // There may be multiple entries if batched
+//     req.body.entry.forEach(function(entry) {
+//       // Iterate over each messaging event
+//       entry.messaging.forEach(function(event) {
+//         if (event.postback) {
+//           processPostback(event);
+//         }
+//       });
+//     });
+
+//     res.sendStatus(200);
+//   }
+// });
+
+var mongodbUri='mongodb://@ds147734.mlab.com:47734/scheduledb';
+
+mongoose.connect(mongodbUri,{
+  useNewUrlParser: true,
+  auth :{
+    user: 'carmit',
+    password:'carmit2001'
+  }
+});
+
+var conn=mongoose.connection;
+conn.on('error',console.error.bind(console, 'connection error:'));
+
+conn.once('open', () => {
+console.log('connected to database')
+});
 
 var Movie = require("./models/movie");
 
@@ -106,7 +72,7 @@ app.listen((process.env.PORT || 5000));
 
 // Server index page
 app.get("/", function (req, res) {
-    res.send("Deployed!");
+    res.send("Server run!");
 });
 
 // Facebook Webhook
@@ -210,9 +176,8 @@ function processMessage(event) {
 }
 
 function findMovie(userId, movieTitle) {
-  let apiKey ='420ff3f7'
-    request(`http://www.omdbapi.com/?type=movie&t=${movieTitle}&apiKey=${apiKey}`, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+    request(`http://www.omdbapi.com/?apiKey=${apiKey}&type=movie&t=${movieTitle}`, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
             var movieObj = JSON.parse(body);
             if (movieObj.Response === "True") {
                 var query = {user_id: userId};
